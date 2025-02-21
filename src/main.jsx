ead3e8c62661b2fb095ce3db6f58f34de8a9e1bc
d-sofiika <soniadrozd2709@gmail.com>
2025-02-21 18:32:15 +0200

    created files

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+
+import ReactDOM from "react-dom/client";
+import './index.css'
+import App from './App.jsx'
+import "modern-normalize";
+import { StrictMode } from "react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
